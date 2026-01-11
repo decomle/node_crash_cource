0a48dc5855883975a056d764e8edf729cfdce164
Dat Phung <thedat230589@gmail.com>
2026-01-11 17:41:23 +0700

    Basic of MongoDB + mongoose package

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require('express');
-const path = require('path');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./src/models/blog');
 
 const app = express();
+
+// Connect to mongodb
+const dbURI = 'mongodb+srv://dat_relearn:Com3tom3now@decomle.cbo12nk.mongodb.net/decomle_relearn?appName=decomle';
+mongoose.connect(dbURI)
+    .then((result) => {
+        console.log('connected to db');
+        app.listen(3000)
+    })
+    .catch((err) => {
+        console.log('Unable to connect to the db', err)
+    });
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('69637a4a07d48bf9030602bf')
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log('ERROR', err)
+        })
+})
 
 // register view engine
 app.set('view engine', 'ejs')
 app.set('views', 'templates')
-
-app.listen(3000)
 
 // Static middleware
 app.use(express.static('static'))
@@ -17,14 +37,18 @@ app.use(express.static('static'))
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'What the hell', snippet: 'Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker'},
-        {title: 'Where am I', snippet: 'Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker'},
-        {title: 'Why is it', snippet: 'Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker'},
-    ]
-    res.render('index', {title: 'Home', blogs});
+    res.redirect('/blogs')
 });
 
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createAt: -1})
+        .then((results) => {
+            res.render('index', {title: 'All blogs', blogs:     results})
+        })
+        .catch((err) => {
+            console.log('ERROR', err)
+        })
+});
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
